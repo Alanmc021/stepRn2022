d3611077231561ab112cdbd13bea1b2f5530d622
Alan Martins <alanmartins@mini-de-alan.home>
2022-08-23 19:14:38 -0300

    options

---
+++ src/pages/slideIntro/data.js
@@ -0,0 +1,13 @@
+export const slides = [
+    {
+        id: "1",
+        title: "slide01",
+        uri: 'www.teste.com',
+
+    },
+    {
+        id: "2",
+        title: "slide01",
+        uri: 'www.teste.com',
+    }
+]
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,20 +3,29 @@ import { View, Text, Button } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
-//tela 1
+
 function HomeScreen({ navigation }) {
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <Text>Home</Text>
-            <Button title="Button" onPress={() => { navigation.navigate('Detail') }} />
+            <Button title="Button" onPress={() => {
+                navigation.navigate('Detail', {
+                    name: 'Alan',
+                    valor: 10,
+                    msg: 'Esse dado veio de outra pagina'
+                })
+            }} />
         </View>
     )
 }
-//tela 2
-function DetailsScreen() {
+
+function DetailsScreen({ route }) {
+    const { name, valor, msg } = route.params
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Detail</Text>
+            <Text>Detail {name}</Text>
+            <Text>Detail {valor}</Text>
+            <Text>Detail {msg}</Text>
         </View>
     )
 }
@@ -28,7 +37,14 @@ export default function App() {
     return (
         <NavigationContainer>
             <Stack.Navigator>
-                <Stack.Screen name="Home"  component={HomeScreen} />
+                <Stack.Screen name="Home" options={{
+                    title: "Pagina principal",
+                    headerShown: false,
+                    headerStyle:{
+                        backgroundColor:'orange'
+                    },
+                    headerTintColor:"red",
+                }} component={HomeScreen} />
                 <Stack.Screen name="Detail" component={DetailsScreen} />
             </Stack.Navigator>
         </NavigationContainer>
